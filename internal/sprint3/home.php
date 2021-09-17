01fc7323ea933d03021db9df239d4e596f5382ab
jasyyyan <yipj1257@gmail.com>
2021-09-17 21:09:13 +1200

    added connection code on home page

---
+++ internal/sprint3/home.php
@@ -0,0 +1,5 @@
+<?php
+$con=mysqli_connect("localhost", "yipja", "messyboat88", "yipja_internalv2");
+if(mysqli_connect_errno()){
+    echo "Failed to connect to MySQL:".mysqli_connect_error(); die();}
+?>
