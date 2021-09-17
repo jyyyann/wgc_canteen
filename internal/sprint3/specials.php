eb0d1392872b2ee2e7dd9032d9cee539d8522e7f
jasyyyan <yipj1257@gmail.com>
2021-09-17 21:16:55 +1200

    added connection code and description on all pages

---
+++ internal/sprint3/specials.php
@@ -0,0 +1,7 @@
+<?php
+$con=mysqli_connect("localhost", "yipja", "messyboat88", "yipja_internalv2");
+if(mysqli_connect_errno()){
+    echo "Failed to connect to MySQL:".mysqli_connect_error(); die();}
+?>
+
+<p> This is the specials page.</p>
