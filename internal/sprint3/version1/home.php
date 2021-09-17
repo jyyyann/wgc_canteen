5b80ea7654d9c3f84d2f4f0db939ec5953f22c40
jasyyyan <yipj1257@gmail.com>
2021-09-17 17:07:32 +1200

    - testing - clarified the name of each page - imported a font

--- internal/sprint3/version1/home.php
+++ internal/sprint3/version1/home.php
@@ -4,3 +4,4 @@ if(mysqli_connect_errno()){
     echo "Failed to connect to MySQL:".mysqli_connect_error(); die();}
 ?>
 
+<p> This is home page. </p>
--- internal/sprint3/version1/information.php
+++ internal/sprint3/version1/information.php
@@ -3,3 +3,5 @@ $con=mysqli_connect("localhost", "yipja", "messyboat88", "yipja_internalv2");
 if(mysqli_connect_errno()){
     echo "Failed to connect to MySQL:".mysqli_connect_error(); die();}
 ?>
+
+<p> This is information page. </p>
