639d2bed93c79ce23aabb0f81ca27f76b8f2d0e2
jasyyyan <yipj1257@gmail.com>
2021-09-19 16:45:11 +1200

    logo and navigation tags added

---
+++ internal/sprint3/home.php
@@ -0,0 +1,38 @@
+<?php
+$con=mysqli_connect("localhost", "yipja", "messyboat88", "yipja_internalv2");
+if(mysqli_connect_errno()){
+    echo "Failed to connect to MySQL:".mysqli_connect_error(); die();}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title> WGC Canteen</title>
+    <meta charset="utf-8">
+    <link rel='stylesheet' type='text/css' href='style.css'>
+</head>
+
+<body>
+<header>
+    <!--header element-->
+    <!--this is adapted from a tutorial video on YouTube by Skillthrive-->
+    <!--link here: https://www.youtube.com/watch?v=PwWHL3RyQgk-->
+
+    <!--logo of wgc-->
+    <a href="https://ibb.co/MVcS3FP">
+        <img class="logo" src="https://i.ibb.co/MVcS3FP/wgclogo.png" width=100 height=100 alt="wgclogo">
+    </a>
+
+    <nav>
+        <!--navigation tabs-->
+        <ul class="nav_tag">
+            <li><a class='page' href='home.php'> HOME</a></li>
+            <li><a class='page' href='specials.php'> SPECIALS</a></li>
+            <li><a class='page' href='menu.php'> MENU</a></li>
+        </ul>
+    </nav>
+</header>
+</body>
+</html>
+
